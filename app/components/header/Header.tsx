7fb996819a4f4653d1b5f16e895f12f19f4160cd
kirushaRetar <118094156+KirushaRetarr@users.noreply.github.com>
2025-10-14 08:14:32 +0500

    add colors & some css

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
-import styles from "./css/Header.module.css";
+import logo from "./logo.png"
+import "./css/header.css"
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex flex-row justify-between items-center">
       <div className="flex flex-row justify-start items-center gap-[10px]">
-        <Image src="/img/logo.png" alt="Logo" width={50} height={50}/>
+        <Image src={logo} alt="Logo" width={50} height={50}/>
         <p>Cash Flipper</p>
       </div>
+      <nav>
+        <Link href="/">
+          <p>Главная</p>
+        </Link>
+      </nav>
     </header>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/header/Header";
-import "./globals.css"
+import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,8 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
-        {children}
+        <main>
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
